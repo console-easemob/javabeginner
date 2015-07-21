df21d783809428913728cf255e28f63c44f4e65a
dengchao <wuxiangdc@users.noreply.github.com>
2015-07-21 17:48:17 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,7 @@ public class HelloWorld {
  *         * @param args
  *             */
     public static void main(String[] args) {
-/*    System.out.println("Hello World!");
- */
+      System.out.println("Hello World!");
       logger.log("Hello World!");
     }
 }
